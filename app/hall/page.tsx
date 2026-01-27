5b996130608568c598a348a9e0e89783b336b845
Brikuus <rosmann.henri16@gmail.com>
2026-01-27 11:49:36 +0100

    structure page intro done, navigation vers la page suivante à faire + font à mettre

---
+++ app/hall/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import {Button} from "@/components/ui/8bit/button";
+
+
+export default function Hall() {
+  return (
+    <div>
+      <main>
+      </main>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,48 @@
-import Image from "next/image";
 import {Button} from "@/components/ui/8bit/button";
 
 
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    const texte = `Une commande arrive sur ton téléphone.
+
+                        Un Tasty Crousty encore chaud à livrer.
+
+                        Adresse indiquée :
+                            - Manoir de Valombre.
+
+                        Tu arrives devant une immense porte en bois sombre.
+                        Le manoir est silencieux.
+
+                        Tu lèves la main pour frapper.
+                        La porte s’ouvre toute seule.
+
+                        Avant même que tu puisses reculer, une force invisible t’aspire à l’intérieur.
+                        Tu perds l’équilibre.
+                        Le sac tombe au sol.
+
+                        BOUM.
+
+                        . . .
+
+                        La porte se referme brutalement derrière toi.
+                        Tu te retournes.
+
+                        La poignée ne bouge pas, un boîtier est fixé au centre de la porte.
+                        Un écran rouge s’allume.
+
+                        ce manoir ne te laissera pas sortir sans la combinaison.`;
+    return (
+        <div className="page">
+            <div className="background">
+                <div className="overlay" />
+            </div>
+
+            <div className="content">
+                <h1>Introduction ...</h1>
+                    <p style={{ whiteSpace: "pre-line" }}>
+                        {texte}
+                    </p>
+            </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
