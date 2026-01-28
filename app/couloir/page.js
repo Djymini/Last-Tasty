3de1d4840208e28714b1d2a6741302249916242b
Brikuus <rosmann.henri16@gmail.com>
2026-01-28 02:00:50 +0100

    info bulle apparaissent dans l'inspecteur mais cest tt

--- app/couloir/page.js
+++ app/couloir/page.js
@@ -4,145 +4,148 @@ import {InfoBubble} from "@/components/ui/InfoBubble";
 
 export default function couloir() {
     return (
-        <div className="page"
+        <main className="page"
              style={{
+                 position: "relative",
                  height: "100vh",
+                 backgroundImage: `url("/couloir.png")`,
+                 backgroundSize: "cover",
+                 backgroundPosition: "center",
                  overflow: "hidden",
              }}>
 
-            <div className="background"
+            <section className="doors"
             style={{
-                position: "fixed",
-                inset: "0",
-                zIndex: "0",
-                backgroundImage: `url("../../public/couloir.png")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center center",
+                top: "7vh",
+                position: "absolute",
+                width: "100vw",
+                height: "80vh",
             }}>
 
-                <section className="doors"
-                style={{
-                    top: "5vh",
-                    position: "absolute",
-                    width: "100vw",
-                    height: "80vh",
-                }}>
+                <div className="door1"
+                     style={{
+                         position: "absolute",
+                         cursor: "pointer",
+                         top: "5vh",
+                         left: "12vw",
+                         width: "9vw",
+                         height: "75vh"
 
-                    <div className="door1"
-                         style={{
-                             cursor: "pointer",
-                             top: "7vh",
-                             left: "12vw",
-                             width: "9vw",
-                             height: "75vh"
+                     }}>
+                    <InfoBubble
+                        title="Porte"
+                        description="Cette porte est fermée"
+                        className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
+                        style={{
+                            position: "absolute",
+                            zIndex: "1",
+                            top: "40vh",
+                            left: "40vw",
+                            width: "40vw",
+                            pointerEvents: "none",
+                        }}
+                    />
+                </div>
 
-                         }}>
-                        <InfoBubble
-                            title="Porte"
-                            description="Cette porte est fermée"
-                            className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
-                            style={{
-                                position: "absolute",
-                                top: "12px",
-                                left: "12px",
-                                width: "220px",
-                                pointerEvents: "none",
-                            }}
-                        />
-                    </div>
+                <div className="door2"
+                     style={{
+                         position: "absolute",
+                         cursor: "pointer",
+                         top: "16vh",
+                         left: "28vw",
+                         width: "6vw",
+                         height: "51vh"
 
-                    <div className="door2"
-                         style={{
-                             cursor: "pointer",
-                             top: "18vh",
-                             left: "28vw",
-                             width: "6vw",
-                             height: "51vh"
+                     }}>
+                    <InfoBubble
+                        title="Porte"
+                        description="Cette porte doit menée à la chambre d'un domestique"
+                        className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
+                        style={{
+                            position: "absolute",
+                            zIndex: "1",
+                            top: "40vh",
+                            left: "40vw",
+                            width: "40vw",
+                            pointerEvents: "none",
+                        }}
+                    />
+                </div>
 
-                         }}>
-                        <InfoBubble
-                            title="Porte"
-                            description="Cette porte doit menée à la chambre d'un domestique"
-                            className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
-                            style={{
-                                position: "absolute",
-                                top: "12px",
-                                left: "12px",
-                                width: "220px",
-                                pointerEvents: "none",
-                            }}
-                        />
-                    </div>
+                <div className="door3"
+                     style={{
+                         position: "absolute",
+                         cursor: "pointer",
+                         top: "22vh",
+                         left: "43vw",
+                         width: "14vw",
+                         height: "37vh"
 
-                    <div className="door3"
-                         style={{
-                             cursor: "pointer",
-                             top: "24vh",
-                             left: "43vw",
-                             width: "14vw",
-                             height: "37vh"
+                     }}>
+                    <InfoBubble
+                        title="Porte"
+                        description="Cette porte doit menée au bureau"
+                        className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
+                        style={{
+                            position: "absolute",
+                            zIndex: "1",
+                            top: "40vh",
+                            left: "40vw",
+                            width: "40vw",
+                            pointerEvents: "none",
+                        }}
+                    />
+                </div>
 
-                         }}>
-                        <InfoBubble
-                            title="Porte"
-                            description="Cette porte doit menée au bureau"
-                            className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
-                            style={{
-                                position: "absolute",
-                                top: "12px",
-                                left: "12px",
-                                width: "220px",
-                                pointerEvents: "none",
-                            }}
-                        />
-                    </div>
+                <div className="door4"
+                     style={{
+                         position: "absolute",
+                         cursor: "pointer",
+                         top: "16vh",
+                         right: "28vw",
+                         width: "6vw",
+                         height: "51vh"
 
-                    <div className="door4"
-                         style={{
-                             cursor: "pointer",
-                             top: "18vh",
-                             right: "28vw",
-                             width: "6vw",
-                             height: "51vh"
+                     }}>
+                    <InfoBubble
+                        title="Porte"
+                        description="Cette porte doit menée au rez de chaussé"
+                        className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
+                        style={{
+                            position: "absolute",
+                            zIndex: "1",
+                            top: "40vh",
+                            left: "40vw",
+                            width: "40vw",
+                            pointerEvents: "none",
+                        }}
+                    />
+                </div>
+                <div className="door5"
+                     style={{
+                         position: "absolute",
+                         cursor: "pointer",
+                         top: "5vh",
+                         right: "12vw",
+                         width: "9vw",
+                         height: "75vh"
 
-                         }}>
-                        <InfoBubble
-                            title="Porte"
-                            description="Cette porte doit menée au rez de chaussé"
-                            className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
-                            style={{
-                                position: "absolute",
-                                top: "12px",
-                                left: "12px",
-                                width: "220px",
-                                pointerEvents: "none",
-                            }}
-                        />
-                    </div>
-                    <div className="door1"
-                         style={{
-                             cursor: "pointer",
-                             top: "7vh",
-                             right: "12vw",
-                             width: "9vw",
-                             height: "75vh"
-
-                         }}>
-                        <InfoBubble
-                            title="Porte"
-                            description="Cette porte doit menée à la chambre principal"
-                            className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
-                            style={{
-                                position: "absolute",
-                                top: "12px",
-                                left: "12px",
-                                width: "220px",
-                                pointerEvents: "none",
-                            }}
-                        />
-                    </div>
-                </section>
-            </div>
-        </div>
+                     }}>
+                    <InfoBubble
+                        title="Porte"
+                        description="Cette porte doit menée à la chambre principal"
+                        className="opacity-0 translate-y-1 transition-all duration-150 group-hover:opacity-100 group-hover:translate-y-0"
+                        style={{
+                            position: "absolute",
+                            zIndex: "1",
+                            top: "40vh",
+                            left: "40vw",
+                            width: "40vw",
+                            pointerEvents: "none",
+                        }}
+                    />
+                </div>
+            </section>
+        </main>
     );
 }
